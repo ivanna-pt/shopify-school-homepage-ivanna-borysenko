2bb318d3b7a55a7fabcb26231e65cdac63261ff7
ivanna-pt <ivanna.pokhyl@gmail.com>
2025-08-21 19:03:15 +0300

    Add Product Card section and render data to it

--- js/index.js
+++ js/index.js
@@ -26,6 +26,107 @@ function toggleAccordion(header, content, toggleSelector) {
   content.style.maxHeight = isOpen ? null : content.scrollHeight + 'px';
 }
 
+const productData = {
+  id: "product1",
+  title: "Nike Air Max Plus",
+  description: "Let your attitude have the edge in your Nike Air Max Plus, a Tuned Air experience that offers premium stability and unbelievable cushioning. ",
+  defaultColor: "white",
+  variants: {
+    white: {
+      price: 280.00,
+      images: ["img/product/white-sneakers-side-view.webp", "img/product/white-sneakers-bottom-view.webp", "img/product/white-sneakers-side-view-2.webp", "img/product/white-sneakers-top-view.webp", "img/product/white-sneakers-front-view.webp"],
+      sizes: ['UK 5.5', 'UK 6(EU 39)', 'UK 6(EU 40)', 'UK 6.5', 'UK 7', 'UK 7.5']
+    },
+    black: {
+      price: 320.00,
+      images: [],
+      sizes: ['UK 5.5', 'UK 6(EU 39)', 'UK 6(EU 40)', 'UK 6.5', 'UK 7', 'UK 7.5']
+    },
+    pink: {
+      price: 260.00,
+      images: [],
+      sizes: ['UK 5.5', 'UK 6(EU 39)', 'UK 6(EU 40)', 'UK 6.5', 'UK 7', 'UK 7.5']
+    }
+  }
+
+
+
+}
+
+let selectedColor = productData.defaultColor;
+let selectedSize = null;
+let mainImage = productData.variants[selectedColor].images[0];
+
+const card = document.querySelector('.product-card');
+const mainImgEl = card.querySelector('.main-image img');
+const galleryEl = card.querySelector('.product-gallery');
+const titleEl = card.querySelector('.product-title');
+const priceEl = card.querySelector('.product-price');
+const descriptionEl = card.querySelector('.product-description');
+const colorsEl = card.querySelector('.color-options');
+const sizesEl = card.querySelector('.size-options');
+const addToCartBtn = card.querySelector('.add-to-cart');
+
+function renderProductCard () {
+  const variant = productData.variants[selectedColor];
+  console.log(variant)
+  const {title, description, id} = productData;
+  titleEl.textContent = title;
+  priceEl.textContent = `$ ${variant.price}`;
+  descriptionEl.textContent = description;
+
+  mainImgEl.src = mainImage;
+  mainImgEl.alt = `${title} - ${selectedColor}`;
+
+  galleryEl.innerHTML = '';
+  variant.images.forEach(image => {
+    const imgEl = document.createElement('img');
+    imgEl.src = image;
+    imgEl.alt = `${title} - ${selectedColor}`;
+    imgEl.classList.add('gallery-img');
+    if (image === mainImage) imgEl.classList.add('active');
+    imgEl.addEventListener('click', () => {
+      mainImage.src = image;
+      renderProductCard();
+    })
+    galleryEl.appendChild(imgEl);
+  })
+
+  colorsEl.innerHTML = '';
+  Object.keys(productData.variants).forEach(color => {
+    const btn = document.createElement('button');
+    btn.textContent = color;
+    btn.classList.add('color-btn')
+    if (color === mainImage) btn.classList.add('active');
+    btn.addEventListener('click', () => {
+      selectedColor = color;
+      mainImage = productData.variants[color].images[0];
+      selectedSize = null;
+      renderProductCard();
+    })
+    colorsEl.appendChild(btn);
+  })
+
+  sizesEl.innerHTML = ''
+  variant.sizes.forEach(size => {
+    const btn = document.createElement('button');
+    btn.textContent = size;
+    btn.classList.add('size-btn')
+    if (size === selectedSize) btn.classList.add('active');
+    btn.addEventListener("click", () => {
+      selectedSize = size;
+      renderProductCard();
+    });
+    sizesEl.appendChild(btn);
+  })
+
+
+
+}
+
+
+
+
 const currentYear = new Date().getFullYear();
 document.getElementById('copyrightYear').innerText = currentYear;
 
@@ -37,5 +138,7 @@ navigationBtn.addEventListener('click', ()=> {
   toggleAccordion(navigation, navigation, 'show')
 })
 
+
 setupAccordion('.footer-menu', '.footer-nav-title', '.footer-nav-list');
 setupAccordion('.question-item', '.question-title', '.answer-text');
+renderProductCard();
