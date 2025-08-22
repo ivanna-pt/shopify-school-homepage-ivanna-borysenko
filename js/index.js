7195934ee43a53c069e15446e53a77d776bbb0c5
ivanna-pt <ivanna.pokhyl@gmail.com>
2025-08-22 15:07:35 +0300

    Set up Hero Slider swiper

--- js/index.js
+++ js/index.js
@@ -1,3 +1,28 @@
+const HeroSwiper = new Swiper('.swiper.hero-slider', {
+  speed: 400,
+  spaceBetween: 100,
+  autoplay: {
+    delay: 5000,
+  },
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
+
+
+
+
 function setupAccordion(containerSelector, itemSelector, contentSelector) {
   const accordions = document.querySelectorAll(containerSelector);
   accordions.forEach(accordion => {
